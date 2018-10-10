a60ed4d679e84e34b10a5c1aadd56bf2dd2224a5
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-10-10 10:17:25 +0200

    Spring config

---
+++ src/main/java/com/esme/spring/faircorp/FaircorpApplicationConfig.java
@@ -0,0 +1,21 @@
+package com.esme.spring.faircorp;
+
+import com.esme.spring.faircorp.hello.GreetingService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FaircorpApplicationConfig {
+
+    @Bean
+    public CommandLineRunner greetingCommandLine(GreetingService greetingService) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                greetingService.greet("Spring");
+            }
+        };
+    }
+
+}
