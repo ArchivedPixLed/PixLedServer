2c7eec9cd466e0df3967982de0387365dd40c1b6
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-11-14 09:51:47 +0100

    Implement dto

---
+++ src/main/java/com/esme/spring/faircorp/model/light/dto/LightController.java
@@ -0,0 +1,67 @@
+package com.esme.spring.faircorp.model.light.dto;
+
+import com.esme.spring.faircorp.model.Status;
+import com.esme.spring.faircorp.model.light.Light;
+import com.esme.spring.faircorp.model.light.dao.LightDao;
+import com.esme.spring.faircorp.model.room.dao.RoomDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController  // (1)
+@RequestMapping("/api/lights") // (2)
+@Transactional // (3)
+public class LightController {
+
+    @Autowired
+    private LightDao lightDao; // (4)
+    @Autowired
+    private RoomDao roomDao;
+
+
+    @GetMapping // (5)
+    public List<LightDto> findAll() {
+        return lightDao.findAll()
+                .stream()
+                .map(LightDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(path = "/{id}")
+    public LightDto findById(@PathVariable Long id) {
+        return lightDao.findById(id).map(light -> new LightDto(light)).orElse(null);
+    }
+
+    @PutMapping(path = "/{id}/switch")
+    public LightDto switchStatus(@PathVariable Long id) {
+        Light light = lightDao.findById(id).orElseThrow(IllegalArgumentException::new);
+        light.setStatus(light.getStatus() == Status.ON ? Status.OFF: Status.ON);
+        return new LightDto(light);
+    }
+
+    @PostMapping
+    public LightDto create(@RequestBody LightDto dto) {
+        Light light = null;
+        if (dto.getId() != null) {
+            light = lightDao.findById(dto.getId()).orElse(null);
+        }
+
+        if (light == null) {
+            light = lightDao.save(new Light(dto.getLevel(), dto.getStatus(), roomDao.getOne(dto.getRoomId())));
+        } else {
+            light.setLevel(dto.getLevel());
+            light.setStatus(dto.getStatus());
+            lightDao.save(light);
+        }
+
+        return new LightDto(light);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable Long id) {
+        lightDao.deleteById(id);
+    }
+}
---
+++ src/main/java/com/esme/spring/faircorp/model/light/dto/LightDto.java
@@ -0,0 +1,36 @@
+package com.esme.spring.faircorp.model.light.dto;
+
+import com.esme.spring.faircorp.model.Status;
+import com.esme.spring.faircorp.model.light.Light;
+
+public class LightDto {
+    private Long id;
+    private Integer level;
+    private Status status;
+    private Long roomId;
+
+    public LightDto() {
+    }
+
+    public LightDto(Light light) {
+        this.id = light.getId();
+        this.level = light.getLevel();
+        this.status = light.getStatus();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+}
